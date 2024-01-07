4026d57f36a0ca9b64fd091b11e82f6ac4c75101
rfloarea <account@rfloarea.net>
2024-01-07 17:49:49 -0500

    Clean up pseudocode and write getInputs helper function

--- v1script.js
+++ v1script.js
@@ -1,32 +1,54 @@
-// Pseudocode: version 1 - essential game loop functions
+// --- Pseudocode --- version 1: essential game loop functions
 
-// Human chooses "rock", "paper", or "scissors" via input
-// Computer chooses "rock", "paper", or "scissors" via random selection
-// The choices are compared
-// A winner and loser are declared
-// Repeat this cycle 5 times to complete the game loop
+// Step 1: Get inputs
+    // 1a: Prompt player1 for inputs: "rock", "paper", or "scissors"
+    // 1b: Generate a random number for player2 (computer) within set range
+        // 1b1: assign one number to one value: "rock", "paper", or "scissors"
+        // if randomNumber == 0, then player2Input = rock;
+        // if randomNumber == 1, then player2Input = paper;
+        // if randomNumber == 2, then player3Input = scissors;
+// Step 2: Compare inputs
+    // if player1Input > player2Input, then return winner = player1;
+    // if player1Input < player2Input, then return winner = player2;
+    // if player1Input == player2Input, then return winner = "You tied!";
+// Step 3: Print output
+    // if winner = player1 print "You win!"
+    // if winner = player2, print "You lose."
+    // if player1 output = player2 output, print, "You tied!"
+// Repeat steps 1-3 until five cycles are completed
 
-// --- okay, let's do this ---
+// --- Code ---
 
-// initialize variables
-let rock = "rock"
-let paper = "paper"
-let scissors = "scissors"
+// Step 1: Get inputs
+let player1Input = prompt("Choose rock, paper, or scissors")
+console.log(`You chose:`, player1Input);
 
-// prompt user for input
-let userInput = prompt("Choose rock, paper, or scissors")
-console.log(`You chose:`, userInput)
+let player2Input = Math.floor(Math.random() * 3)
+console.log(player2Input);
 
-// get computer to generate a random number between 0, 2
-let computerInput = Math.floor(Math.random() * 3)
-console.log(computerInput)
-// convert computer input (number) to rock, paper, scissors
-if (computerInput == 0){
-    console.log("Computer chose: rock");
-}   else if (computerInput == 1){
-    console.log("Computer chose: paper");
-}   else if (computerInput == 2){
-    console.log("Computer chose: scissors")
+if (player2Input == 0){
+    player2Input = "rock";
+    console.log(player2Input);
+} else if (player2Input == 1){
+    player2Input = "paper";
+    console.log(player2Input);
+} else if (player2Input == 2){
+    player2Input = "scissors";
+    console.log(player2Input);
 }
 
-// Compare the user input vs computer input
+// Step 2: Compare inputs
+
+let winner = null;
+let player1 = "You"
+let player2 = "I"
+
+if (player1Input == "rock" && player2Input == "rock"){
+    winner = null;
+} else if (player1Input == "rock" && player2Input == "paper"){
+    winner = player2;
+} else if (player1Input == "rock" && player2Input == "scissors"){
+    winner = player1;
+}
+
+console.log(winner)
