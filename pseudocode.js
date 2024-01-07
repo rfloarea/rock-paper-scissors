4de173229d30eebbd7faa37dbba575f32613f8fc
rfloarea <account@rfloarea.net>
2024-01-07 14:02:07 -0500

    Write pseudocode for essential game loop functions

---
+++ pseudocode.js
@@ -0,0 +1,45 @@
+console.log("Rock, Paper, Scissors")
+
+// Notes:
+// This is essentially a "value comparison" game
+// One player chooses a value
+// The other player chooses a value
+// The values are evaluated and compared based on game rules
+// A winner and loser are declared
+
+// ---------------------------------------------------------------------
+
+// Pseudocode: version 1 - essential game loop functions
+
+// Human chooses "rock", "paper", or "scissors" via input
+// Computer chooses "rock", "paper", or "scissors" via random selection
+// The choices are compared
+// A winner and loser are declared
+// Repeat this cycle 5 times to complete the game loop
+
+// ---------------------------------------------------------------------
+
+// Pseudocode: version 2 - game loop plus interaction
+
+// Print, "Do you want to start a game?"
+// User inputs, "Yes" or "No"
+// If yes, go to START GAME
+// If no, exit 
+// START GAME
+// START ROUND
+// User inputs choice of "rock", "paper", or "scissors"
+// Computer randomly chooses
+// Print the user and computer's choices
+// If user wins, print "You win this round!"
+// If computer wins, print "You lose this round."
+// End round
+// Repeat until 5 rounds have been played
+// The winner of the whole game is who won the most rounds
+// If user wins more than computer, print "You won the game!"
+// If user wins less than computer, print "You lost the game."
+// Print, "Do you want to play another game?"
+// User inputs, "Yes" or "No"
+// If yes, clear results and start round
+// If no, exit game
+
+// ---------------------------------------------------------------------
