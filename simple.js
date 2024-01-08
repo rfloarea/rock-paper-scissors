9f58086c49e8fabba12fbcc2c1e1f416b4a54934
rfloarea <account@rfloarea.net>
2024-01-08 17:43:08 -0500

    FIX: print computerChoice

--- simple.js
+++ simple.js
@@ -1,75 +1,64 @@
-// let's keep things super fucking simple
-// and maybe follow the directions for once
+// --------- ??? --------- //
+// roundWinner is not being reassigned after playRound()
+// playerScore is not being incremented after playRound()
+// computerScore is not being incremented after playRound()
 
-// first things first, we need to initialize some globals
 
 let playerScore = 0
 let computerScore = 0
 let roundWinner = ``
-let playerChoice = ``
-let computerChoice = 0
+let playerChoice = getPlayerChoice();
+let computerChoice = getComputerChoice();
 
-// now we need a function to get the computer's choice
+playRound(playerChoice, computerChoice);
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Choose rock, paper, or scissors.").toLowerCase();
+    console.log(playerChoice);
+    return playerChoice;
+}
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice) {
         case 0:
-            return `rock`
+            computerChoice = `rock`;
+            console.log(computerChoice);
+            return;
         case 1:
-            return `paper`
+            computerChoice = `paper`;
+            console.log(computerChoice);
+            return;
         case 2:
-            return `scissors`
+            computerChoice = `scissors`;
+            console.log(computerChoice);
+            return;
     }
 }
 
-console.log(getComputerChoice());
-
-// great! now go get the player's choice!
-
-function getPlayerChoice() {
-    let playerChoice = prompt("Choose rock, paper, or scissors.");
-    playerChoice = playerChoice.toLowerCase();
-    return playerChoice;
-}
-
-console.log(getPlayerChoice());
-
-// awesome! you did it! now, let's make them fight!
-
-function playRound(getPlayerChoice, getComputerChoice) {
-    // note: this is like what we already have in the v2 file, but simplified!
+function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         roundWinner = `draw`
     }
-    // if playerChoice beats computerChoice
     else if (
         (playerChoice === `rock` && computerChoice === `scissors`) ||
         (playerChoice === `paper` && computerChoice === `rock`) ||
         (playerChoice === `scissors` && computerChoice === `paper`)
     ) {
-        //then increment playerScore
         playerScore++
-        //and declare the winner of the round!
         roundWinner = `You win!`
     }
-    // now, if the computer's choice beats the player's choice, then...
     else if (
         (computerChoice === `rock` && playerChoice === `scissors`) ||
         (computerChoice === `paper` && playerChoice === `rock`) ||
         (computerChoice === `scissors` && playerChoice === `paper`)
     ) {
-        // then increment computerScore
         computerScore++
-        //and declare them the winner!
         roundWinner = `I win!`
     }
 
-    // now we need to share the news
     console.log(roundWinner)
     console.log(`Your score is: ${playerScore}`)
     console.log(`My score is: ${computerScore}`)
 
 }
-
-playRound();
