eafd8d84cb3267e96f29e50223baabc225f513f8
rfloarea <account@rfloarea.net>
2024-01-09 17:20:16 -0500

    write a function to play a round

--- simple.js
+++ simple.js
@@ -5,7 +5,7 @@ function getPlayerChoice() {
 
     let playerChoice = prompt("Choose rock, paper, or scissors.").toLowerCase();
     
-    console.log(playerChoice);
+    console.log(`You chose: ${playerChoice}`);
     
     return playerChoice;
 }
@@ -17,51 +17,47 @@ function getComputerChoice(computerChoice) {
     
 	if (computerChoice == 0) {
 		computerChoice = "rock";
-        console.log(computerChoice);
+        console.log(`I chose: ${computerChoice}`);
 		return computerChoice;
 	} else if (computerChoice == 1) {
 		computerChoice = "paper";
-        console.log(computerChoice);
+        console.log(`I chose: ${computerChoice}`);
 		return computerChoice;
 	} else {
 		computerChoice = "scissors";
-        console.log(computerChoice);
+        console.log(`I chose: ${computerChoice}`);
 		return computerChoice;
 	}
 }
-
-
-
-// write a function that takes the userChoice and computerChoice and makes them fight
 
 
 let roundWinner = ``;
 let playerScore = 0;
 let computerScore = 0;
 
-// function playRound(playerChoice, computerChoice) {
-//     if (playerChoice === computerChoice) {
-//         roundWinner = `draw`
-//     }
-//     else if (
-//         (playerChoice === `rock` && computerChoice === `scissors`) ||
-//         (playerChoice === `paper` && computerChoice === `rock`) ||
-//         (playerChoice === `scissors` && computerChoice === `paper`)
-//     ) {
-//         playerScore++
-//         roundWinner = `You win!`
-//     }
-//     else if (
-//         (computerChoice === `rock` && playerChoice === `scissors`) ||
-//         (computerChoice === `paper` && playerChoice === `rock`) ||
-//         (computerChoice === `scissors` && playerChoice === `paper`)
-//     ) {
-//         computerScore++
-//         roundWinner = `I win!`
-//     }
+playRound(playerChoice, computerChoice);
 
-//     console.log(roundWinner)
-//     console.log(`Your score is: ${playerScore}`)
-//     console.log(`My score is: ${computerScore}`)
-
-// }
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        roundWinner = `It's a draw.`
+    }
+    else if (
+        (playerChoice === `rock` && computerChoice === `scissors`) ||
+        (playerChoice === `paper` && computerChoice === `rock`) ||
+        (playerChoice === `scissors` && computerChoice === `paper`)
+    ) {
+        ++playerScore
+        roundWinner = `You win!`
+    }
+    else if (
+        (computerChoice === `rock` && playerChoice === `scissors`) ||
+        (computerChoice === `paper` && playerChoice === `rock`) ||
+        (computerChoice === `scissors` && playerChoice === `paper`)
+    ) {
+        ++computerScore
+        roundWinner = `I win!`
+    }
+    console.log(roundWinner)
+    console.log(`Your score is: ${playerScore}`)
+    console.log(`My score is: ${computerScore}`)
+}
