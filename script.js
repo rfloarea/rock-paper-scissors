2092b05e56218a445dbaaf833ee3498788457b48
rfloarea <account@rfloarea.net>
2024-01-17 13:10:08 -0500

    add click event listeners to get player selection

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// create our element selectors
+// create our element selectors that will change as the game is played
 
 const rockBtn = document.getElementById('rockBtn');
 const paperBtn = document.getElementById('paperBtn');
@@ -7,6 +7,15 @@ const scissorBtn = document.getElementById('scissorBtn');
 const playerChoice = document.getElementById('playerChoice');
 const computerChoice = document.getElementById('computerChoice');
 
-const playerScore = document.getElementById('playerScore');
-const computerScore = document.getElementById('computerScore');
-const winnerMessage = document.getElementById('winnerMessage');
+const playerScoreElement = document.getElementById('playerScore');
+const computerScoreElement = document.getElementById('computerScore');
+const winnerMessageElement = document.getElementById('winnerMessage');
+
+// create our event listeners to get the player's selection
+
+rockBtn.addEventListener('click', () => clickEvent('ROCK'));
+paperBtn.addEventListener('click', () => clickEvent('PAPER'));
+scissorsBtn.addEventListener('click', () => clickEvent('SCISSORS'));
+// TODO: create some sort of "play again" button
+
+//
