6c30417b008177b4a6b61d6b4041ed8a8653e144
rfloarea <account@rfloarea.net>
2024-01-17 12:56:08 -0500

    add element selctors for player and computer choices

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const rockBtn = document.getElementById('rockBtn');
 const paperBtn = document.getElementById('paperBtn');
 const scissorBtn = document.getElementById('scissorBtn');
 
+const playerChoice = document.getElementById('playerChoice');
+const computerChoice = document.getElementById('computerChoice');
