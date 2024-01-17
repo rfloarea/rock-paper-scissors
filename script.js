10b1b97ebe0481715bb25d8c4082f8255a37755a
rfloarea <account@rfloarea.net>
2024-01-17 14:07:11 -0500

    add functions to update the choices and scores

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// create our element selectors that will change as the game is played
+// initialize our round values
+let roundWinner = ``;
+let playerScore = 0;
+let computerScore = 0;
 
+// create our element selectors that will change as the game is played
 const rockBtn = document.getElementById('rockBtn');
 const paperBtn = document.getElementById('paperBtn');
 const scissorBtn = document.getElementById('scissorBtn');
@@ -12,23 +16,18 @@ const computerScoreElement = document.getElementById('computerScore');
 const winnerMessageElement = document.getElementById('winnerMessage');
 
 // create our event listeners to get the player's selection
-
 rockBtn.addEventListener('click', () => clickEvent('ROCK'));
 paperBtn.addEventListener('click', () => clickEvent('PAPER'));
 scissorsBtn.addEventListener('click', () => clickEvent('SCISSORS'));
 // TODO: create some sort of "play again" button
 
 // initiate the game loop using our player's choice
-
 function clickEvent(playerChoice) {
   // call our function to get computerChoice and store value as computerChoice
   const computerChoice = getComputerChoice();
   // call our function to play a round using playerChoice and computerChoice as args
   playRound(playerChoice, computerChoice);
-  // call our function to update playerChoiceElement and computerChoiceElement
-  updateChoices(playerChoice, computerChoice);
-  // call our function to update winnerMessageElement, playerScoreElement and computerScoreElement
-  updateScoreBoard(playerScore, computerScore);
+
 }
 
 // generate a choice for the computer
@@ -47,14 +46,10 @@ function getComputerChoice() {
 }
 
 // use our player and computer choices to play one round
-let roundWinner = ``;
-let playerScore = 0;
-let computerScore = 0;
-
 function playRound(playerChoice, computerChoice) {
   // tie results
   if (playerChoice === computerChoice){
-    roundWinner = `It's a tie!`
+    roundWinner = `tie`
   }
   // player win results
   if (
@@ -62,7 +57,7 @@ function playRound(playerChoice, computerChoice) {
     (playerChoice == 'PAPER' && computerChoice == 'ROCK') ||
     (playerChoice == 'SCISSORS' && computerChoice == 'PAPER')
   ) {
-    roundWinner = `You win!`
+    roundWinner = `player`
     playerScore++
   }
   // computer win results
@@ -71,7 +66,24 @@ function playRound(playerChoice, computerChoice) {
     (computerChoice == 'PAPER' && playerChoice == 'ROCK') ||
     (computerChoice == 'SCISSORS' && playerChoice == 'PAPER')
   ) {
-    roundWinner = `You lose :(`
+    roundWinner = `computer`
     computerScore++
   }
+  // call our function to update playerChoiceElement and computerChoiceElement
+  updateChoiceElements(playerChoice, computerChoice);
+  // call our function to update winnerMessageElement, playerScoreElement and computerScoreElement
+  updateScoreBoard(roundWinner, playerScore, computerScore);
+}
+
+function updateChoiceElements(playerChoice, computerChoice) {
+  //update playerChoiceElement
+  playerChoiceElement.textContent = playerChoice;
+  //update computerChoiceElement
+  computerChoiceElement.textContent = computerChoice;
+}
+
+function updateScoreBoard(roundWinner, playerScore, computerScore) {
+  roundWinnerElement.textContent = roundWinner;
+  playerScoreElement.textContent = `Player score: ${playerScore}`;
+  computerScoreElement.textContent = `Computer score: ${computerScore}`;
 }
