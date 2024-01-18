503b8e9771dee3ab8d485655c7a2188562a1d145
rfloarea <account@rfloarea.net>
2024-01-17 19:16:10 -0500

    Impliment a reset function and stuff

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
-// initialize our round values
-let roundWinner = ``;
+let roundWinner = ` `;
 let playerScore = 0;
 let computerScore = 0;
 
-// create our element selectors that will change as the game is played
 const rockBtn = document.getElementById('rockBtn');
 const paperBtn = document.getElementById('paperBtn');
 const scissorBtn = document.getElementById('scissorBtn');
-
 const playerChoiceElement = document.getElementById('playerChoice');
 const computerChoiceElement = document.getElementById('computerChoice');
-
 const playerScoreElement = document.getElementById('playerScore');
 const computerScoreElement = document.getElementById('computerScore');
 const roundWinnerElement = document.getElementById('winnerMessage');
+const playAgainBtn = document.getElementById('modalBtn');
+const modalElement = document.getElementById('modal');
+const modalTextElement = document.getElementById('modalText');
 
-// create our event listeners to get the player's selection
 rockBtn.addEventListener('click', () => clickEvent('ROCK'));
 paperBtn.addEventListener('click', () => clickEvent('PAPER'));
 scissorBtn.addEventListener('click', () => clickEvent('SCISSORS'));
-// TODO: create some sort of "play again" button
+playAgainBtn.addEventListener('click', () => resetGame);
 
-// initiate the game loop using our player's choice
 function clickEvent(playerChoice) {
-  // call our function to get computerChoice and store value as computerChoice
   const computerChoice = getComputerChoice();
-  // call our function to play a round using playerChoice and computerChoice as args
   playRound(playerChoice, computerChoice);
-
+  if (isGameOver()) {
+    resetGame();
+  }
 }
 
-// generate a choice for the computer
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
-  console.log(randomNumber);
 
   switch (randomNumber) {
     case 0:
@@ -45,13 +40,10 @@ function getComputerChoice() {
   }
 }
 
-// use our player and computer choices to play one round
 function playRound(playerChoice, computerChoice) {
-  // tie results
   if (playerChoice === computerChoice){
     roundWinner = `It's a tie`
   }
-  // player win results
   if (
     (playerChoice == 'ROCK' && computerChoice == 'SCISSORS') ||
     (playerChoice == 'PAPER' && computerChoice == 'ROCK') ||
@@ -60,7 +52,6 @@ function playRound(playerChoice, computerChoice) {
     roundWinner = `You won!`
     playerScore++
   }
-  // computer win results
   if (
     (computerChoice == 'ROCK' && playerChoice == 'SCISSORS') ||
     (computerChoice == 'PAPER' && playerChoice == 'ROCK') ||
@@ -69,16 +60,12 @@ function playRound(playerChoice, computerChoice) {
     roundWinner = `You lost`
     computerScore++
   }
-  // call our function to update playerChoiceElement and computerChoiceElement
   updateChoiceElements(playerChoice, computerChoice);
-  // call our function to update winnerMessageElement, playerScoreElement and computerScoreElement
   updateScoreBoard(roundWinner, playerScore, computerScore);
 }
 
 function updateChoiceElements(playerChoice, computerChoice) {
-  //update playerChoiceElement
   playerChoiceElement.textContent = playerChoice;
-  //update computerChoiceElement
   computerChoiceElement.textContent = computerChoice;
 }
 
@@ -86,4 +73,26 @@ function updateScoreBoard(roundWinner, playerScore, computerScore) {
   roundWinnerElement.textContent = `${roundWinner}`;
   playerScoreElement.textContent = `Your score: ${playerScore}`;
   computerScoreElement.textContent = `Computer score: ${computerScore}`;
+}
+
+function isGameOver() {
+  return playerScore == 5 || computerScore == 5
+}
+
+function showModal() {
+  modalElement.classList.add('active');
+  return playerScore > computerScore
+    ? (modalTextElement.textContent = `You won!`)
+    : (modalTextElement.textContent = `You lost`)
+}
+
+function resetGame() {
+  playerScore = 0;
+  playerScoreElement.textContent = `Your score: 0`;
+  computerScore = 0;
+  computerScoreElement.textContent = `Computer score: 0`;
+  playerChoiceElement.textContent = ` `;
+  computerChoiceElement.textContent = ` `;
+  roundWinnerElement.textContent = ` `;
+  modalElement.classList.remove('active');
 }
